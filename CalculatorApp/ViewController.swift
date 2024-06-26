35e5eb4b6f106742d6d6406ec5ff2aa6c6b62dae
RFClimber <choiwoolim98@gmail.com>
2024-06-26 12:44:52 +0900

    LV5 optimization

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -8,20 +8,21 @@
 import UIKit
 import SnapKit
 
-
-
-
 class ViewController: UIViewController {
     
+    // 계산기 레이블 생성
     let numberLabel = UILabel()
     
-    let firstLineButtons = ["7", "8", "9", "+"]
-    let secondLineButtons = ["4", "5", "6", "-"]
-    let thirdLineButtons = ["1", "2", "3", "*"]
-    let fourthLineButtons = ["AC", "0", "=", "/"]
+    // 계산기 버튼 생성 위한 배열 생성
+    let (firstLineButtons, secondLineButtons, thirdLineButtons, fourthLineButtons) = (
+        ["7", "8", "9", "+"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "*"],
+        ["AC", "0", "=", "/"])
     
+    
+    // vStackView 생성
     let vStackView = UIStackView()
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,35 +33,35 @@ class ViewController: UIViewController {
         
         view.backgroundColor = .black
         
+        // hStackView 생성 및 배열에 추가
+        let buttonLines: Array<UIStackView> = [
+            makeHorizontalStackView(firstLineButtons),
+            makeHorizontalStackView(secondLineButtons),
+            makeHorizontalStackView(thirdLineButtons),
+            makeHorizontalStackView(fourthLineButtons)]
         
-        let firstButtonsLine = makeHorizontalStackView(firstLineButtons)
-        let secondButtonsLine = makeHorizontalStackView(secondLineButtons)
-        let thirdButtonsLine = makeHorizontalStackView(thirdLineButtons)
-        let fourthButtonsLine = makeHorizontalStackView(fourthLineButtons)
-        
-        let buttonLines: Array<UIStackView> = [firstButtonsLine, secondButtonsLine, thirdButtonsLine, fourthButtonsLine]
-        
+        // hStackView -> vStackView에 추가
         buttonLines.forEach { vStackView.addArrangedSubview($0) }
         
         // 레이블 속성
+        var num: Int = 0
+        numberLabel.text =  "\(num)"
         numberLabel.backgroundColor = .black
-        numberLabel.text =  "\(12345)"
         numberLabel.textColor = .white
         numberLabel.textAlignment = .right
         numberLabel.font = .boldSystemFont(ofSize: 60)
         
+        // vStackView 속성
         vStackView.axis = .vertical
         vStackView.backgroundColor = .black
         vStackView.spacing = 10
         vStackView.distribution = .fillEqually
         
+        // view에 추가
         [numberLabel, vStackView]
             .forEach { view.addSubview($0) }
         
-        
-    
-        
-        // 라벨 레이아웃
+        // 오토레이아웃
         numberLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(200)
             $0.leading.trailing.equalToSuperview().inset(30)
@@ -78,29 +79,36 @@ class ViewController: UIViewController {
         
         
     }
-
     
+    // 버튼 및 hStackView 생성 함수
     private func makeHorizontalStackView(_ views: [String]) -> UIStackView {
+        
+        // hStackView 생성
         let hStackView = UIStackView()
+        
         hStackView.axis = .horizontal
         hStackView.backgroundColor = .black
         hStackView.spacing = 10
         hStackView.distribution = .fillEqually
         
+        // 버튼 생성 로직
         for title in views {
             let button = UIButton()
+            
             button.setTitle(title, for: .normal)
             button.setTitleColor(.white, for: .normal)
             button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+            button.frame.size.height = 80
+            button.frame.size.width = 80
+            button.layer.cornerRadius = 40
+            
+            // 연산 버튼(+, -, *, /, =, AC) 색상 변경 로직
             if Int(title) ==  nil {
                 button.backgroundColor = UIColor.orange
             } else {
                 button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
             }
-            button.frame.size.height = 80
-            button.frame.size.width = 80
-            button.layer.cornerRadius = 40
-            
+            // 생성된 버튼 hStackView에 삽입
             hStackView.addArrangedSubview(button)
         }
         return hStackView
